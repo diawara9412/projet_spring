873614fc9aca6d542d3f5d4bb0dc66c2d8045dc4
diawara9412 <id2029937@gmail.com>
2025-05-09 02:38:21 +0200

    deux

--- payment/src/main/java/com/example/demo/service/PaymentProcessorFactory.java
+++ payment/src/main/java/com/example/demo/service/PaymentProcessorFactory.java
@@ -6,7 +6,6 @@ import org.springframework.stereotype.Component;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 
-
 @Component
 @Slf4j
 public class PaymentProcessorFactory {
@@ -35,12 +34,20 @@ public class PaymentProcessorFactory {
 
     public PaymentProcessor getProcessor(PaymentMethod paymentMethod) {
         log.debug("Récupération du processeur pour la méthode de paiement: {}", paymentMethod);
+
+        // Vérification si le paymentMethod est null
+        if (paymentMethod == null) {
+            throw new IllegalArgumentException("Unsupported payment method: null");
+        }
+
+        // Récupération du processeur selon la méthode de paiement
         return switch (paymentMethod) {
             case CREDIT_CARD -> creditCardProcessor;
             case PAYPAL -> paypalProcessor;
             case BANK_TRANSFER -> bankTransferProcessor;
             case APPLE_PAY -> applePayProcessor;
             case GOOGLE_PAY -> googlePayProcessor;
+            default -> throw new IllegalArgumentException("Unsupported payment method: " + paymentMethod);
         };
     }
 }
